548b1b6415784ac071da91cd095d6deba75b458a
sayalimahadik <46214399+sayalimahadik@users.noreply.github.com>
2018-12-28 12:58:09 +0530

    Add files via upload
    2 lectures done.

---
+++ km to miles converter.py
@@ -0,0 +1,5 @@
+print("\n\n*********WELCOME*********\n\n")
+print("how many kilometer did you ran:")
+x = int(input())
+z = x * 1.60934 #converted into mile 
+print(f"\nyou ran {z} miles..!!\n")
---
+++ python1.py
@@ -0,0 +1,5 @@
+print("\n\n******WELCOME******\n\n")
+print("How much sugar do you want ?")
+x = int(input())
+y = x * 50
+print(f"\n{x} kg of sugar is of Rs. {y}\n")
