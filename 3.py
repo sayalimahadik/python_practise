e5af80fa2afa52e18a1dc95efe37476e75d7d65a
sayalimahadik <46214399+sayalimahadik@users.noreply.github.com>
2019-01-02 19:38:20 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,18 @@
+print("\n\n\n\n\n********WELCOME TO NEW YEAR PARTY*******\n\n\n\n\n")
+
+print("enter the age of each new person:")
+
+a=int(input())
+
+
+if a>=18 and a<=21:
+	 print("you can take only band,you can't drink")
+
+
+elif a>21:
+	print("you can drink ")
+
+
+else:
+	print("your are so young")
+
---
+++ sacalsi.py
@@ -0,0 +1,29 @@
+print("\n\n\n******this is new calsi*******\n\n\n")
+print("enter the first number:")
+x=int(input())
+print("enter the second number")
+y=int(input())
+print("\naddition of two numbers\n")
+z=x+y
+print(z)
+print("\n subtraction of two numbers\n")
+t=x-y
+print(t)
+print("\nmultiplication of two numbers\n")
+k=x*y
+print(k)
+print("\n***division of two numbers as folllow***\n")
+c=x/y
+print(c)
+print("\n***floor division***\n")
+b=x//y
+print(b)
+print("\n***exponatiation***\n")
+a=x**y
+print(a)
+print("\n***modulus***\n")
+v=x%y
+print(v)
+
+
+
