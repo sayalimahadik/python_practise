da6c42b2b32a7bd532a6db94a395721e2770ca86
sayalimahadik <46214399+sayalimahadik@users.noreply.github.com>
2019-01-02 19:59:23 +0530

    Add files via upload

--- guessing game.py
+++ guessing game.py
@@ -1,4 +1,13 @@
 import random
-
-r = random.randint(1,10)
+r = random.randint(1,20)
 print(r)
+print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+print("Enter the number:")
+x=int(input())
+
+if x<r:
+	print("number is very less as campare to r")
+elif x==r:
+	print("number is same")
+else:
+	print("number is larger")
