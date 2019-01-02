a8033565194889273409e3986f5949f1fa7bdbaa
sayalimahadik <46214399+sayalimahadik@users.noreply.github.com>
2019-01-02 20:15:40 +0530

    Update table.py

--- table.py
+++ table.py
@@ -1,4 +1,14 @@
+print("enter the number:")
+z=int(input())
 for x in range(1,11):
-	for y in range(1,11):
-		print(f"{x} X {y} = ", x*y)
-	print("\n")
+	print(f"{x} X {z} = ", x*z)
+
+
+
+
+
+
+#for x in range(1,11):
+#	for y in range(1,11):
+#		print(f"{x} X {y} = ", x*y)
+#	print("\n")
