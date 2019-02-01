fb67fd17f0d666d3c23537fa89010620441026bc
sayalimahadik <46214399+sayalimahadik@users.noreply.github.com>
2019-02-01 21:36:01 +0530

    Add files via upload
    1/2/2019

---
+++ converter.py
@@ -0,0 +1,10 @@
+print("\nWELCOME TO CONVERTER\n")
+x=int(input("enter the input: "))
+z=0.3937*x    #convertered into inches
+print(f"\n{x} cm convert into {z} inch\n")
+
+v=float(input("enter the input: "))
+z=2.54*v    #convertered into centimeter
+print(f"\n{v} inches convert into {z} centimeter\n")
+
+
