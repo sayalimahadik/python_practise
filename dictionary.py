5d705c30cb7833c4cd9d09c780cb42380aa97098
sayalimahadik <46214399+sayalimahadik@users.noreply.github.com>
2019-02-03 14:55:56 +0530

    Add files via upload
    03/02/2019

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+x={"riya":23,"sayali":23,"diya":12,"sujla":78}
+# print(x)
+# print("\n")
+y=x.items()
+print(y)
+print("\n")
+for i in x:
+	print("items: ",i)
+print("\n")
+z=x.values()
+print(z)
+print("\n")
+for i in x.values():
+	print("values: ",i)
+print("\n")
