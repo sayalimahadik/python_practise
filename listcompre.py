d75538b5a93bbee8e8106e528043c09b77c3a52e
sayalimahadik <46214399+sayalimahadik@users.noreply.github.com>
2019-02-03 14:18:13 +0530

    Add files via upload
    3/2/19

---
+++ listcompre.py
@@ -0,0 +1,46 @@
+x=[1,2,3,4,5,6,7,8,9]
+# y=[i*10 for i in x]
+# print(y)
+# print("\n")
+# v=[t**2 for t in x]
+# print(v)
+# print("\n")
+# v=[z**3 for z in x]
+# print(v)
+# print("\n")
+# y=[]
+# z=[]
+# for i in x:
+# 	if i%2==0:
+# 		y.append(i)
+# 		# print(f"{y} number is even")
+# 	else:
+# 		z.append(i)
+# 		# print(f"{z} number is odd")
+# print(y)
+# print(z)
+# z=[s*2 for s in x if s%2==0]
+# print(z)
+# y=[]
+# z=[]
+# for i in x:
+# 	if i%2==0:
+# 		y.append(i/2)
+# 		print(f"{y}")
+# 	else:
+# 		z.append(i*2)
+# 		print(f"{z}")
+
+
+
+# t=[n/2 if n%2==0 else n*2 for n in x]
+# print(t)
+
+# y=[]
+# z=[]
+# [y.append(n/2) if n%2==0 else z.append(n*2) for n in x]
+# print(y)
+# print(z)
+
+				
+
