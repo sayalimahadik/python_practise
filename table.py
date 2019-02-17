6606877614dee29505b35101f1ac4ac17f39597c
sayalimahadik <46214399+sayalimahadik@users.noreply.github.com>
2019-02-17 13:55:31 +0530

    Add files via upload
    17/2/019 while loop

--- table.py
+++ table.py
@@ -1,14 +1,28 @@
-print("enter the number:")
-z=int(input())
-for x in range(1,11):
-	print(f"{x} X {z} = ", x*z)
+# print("enter the input: ")
+# a=int(input())
+# i=1
+# while i<=10:
+# 	print(f"{i}X{a}= ",i*a)
+# 	i+=1
 
 
+# i=0
+# while i<10:
+# 	# print(i)
+# 	i+=1
+# 	x=0
+# 	while x<10:
+# 		# print(x)
+# 		x+=1
+# 		print(f"{i}X{x}= ",i*x)
+# 	print("\n")
 
 
-
-
-#for x in range(1,11):
-#	for y in range(1,11):
-#		print(f"{x} X {y} = ", x*y)
-#	print("\n")
+i=0
+while i<20:
+	i+=1
+	x=0
+	while x<10:
+		x+=1
+		print(f"{i} X {x} = ",i*x)
+	print("\n")
---
+++ wfact.py
@@ -0,0 +1,9 @@
+i=1
+c=1
+
+print("enter the number:")
+a=int(input())
+while i<=a:
+	c=c*i
+	i+=1
+print(c)
