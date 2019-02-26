fe70d6bf2bb23e322955d5c5476d0b657eb457f1
sayalimahadik <46214399+sayalimahadik@users.noreply.github.com>
2019-02-26 21:11:45 +0530

    Add files via upload

---
+++ table 1.py
@@ -0,0 +1,9 @@
+print("enter the number:")
+z=int(input())
+for x in range(1,11):
+	print(f"{x} X {z} = ", x*z
+
+#for x in range(1,11):
+#	for y in range(1,11):
+#		print(f"{x} X {y} = ", x*y)
+#	print("\n")
