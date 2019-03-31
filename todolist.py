bb7c5d9f005e60d4429b9c28ef2154887bd2aaf4
sayalimahadik <46214399+sayalimahadik@users.noreply.github.com>
2019-03-30 23:49:33 -0700

    Add files via upload
    31/01/019

---
+++ todolist.py
@@ -0,0 +1,25 @@
+def todo_list():
+	print("enter your input: ")
+	x=input()
+	print("enter your second input: ")
+	
+	for z in range(1,6):
+		y=input()
+		
+		
+		
+		if y=="quit":
+			break
+		else:
+			continue
+
+todo_list()
+	
+
+	
+
+
+
+
+
+		
