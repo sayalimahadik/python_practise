caa447063d16c029cd6a610921bf00fd2920b5c5
sayalimahadik <46214399+sayalimahadik@users.noreply.github.com>
2019-04-07 12:04:48 +0530

    Add files via upload
    7/4/019

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def calsi():	
+	print("enter the first input: ")
+	x=int(input())
+	print("enter the second input: ")
+	y=int(input())
+	z=x+y
+	print(z)
+	c=x-y
+	print(c)
+	p=x/y
+	print(p)
+	t=x*y
+	print(t)
+	w=x//y
+	print(w)
+calsi()
